57df04ec28236e320482bdb84e4add97328eb046
hollyword221 <lena.0221.96@gmail.com>
2023-01-29 03:20:26 -0800

    init

---
+++ res/shaders/aufgabe1.glsl
@@ -0,0 +1,12 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+#version 330 core
+
+out vec4 color;
+
+void main(void) {
+
+  color = vec4(0.9, 0.3, 0.9, 1.0);
+}
